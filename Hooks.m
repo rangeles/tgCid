83f9a92a0d6d01ede402ee450ccca12da8eeee3b
Ron Angeles <ron.angeles@gmail.com>
2011-11-06 09:34:22 -0800

    More hook fixes. Just push teardowns when the dealloc message is received. Send ABUID when the top bar is requested.

--- Hooks.m
+++ Hooks.m
@@ -7,53 +7,28 @@ static CallManager *manager;
 CHDeclareClass(MPIncomingPhoneCallController);
 
 CHOptimizedMethod(1, self, id, MPIncomingPhoneCallController, initWithCall, CTCallRef, call) {
-    NSLog(@"-[<MPIncomingPhoneCallController: %p> initWithCall:%@]", self, call);
-    id ret = CHSuper(1, MPIncomingPhoneCallController, initWithCall, call);
-    [manager setupForController:ret withCall:call];
-    return ret;
+    id orig = CHSuper(1, MPIncomingPhoneCallController, initWithCall, call);
+    [manager setupForController:orig withCall:call];
+    return orig;
 }
 
-CHOptimizedMethod(1, self, void, MPIncomingPhoneCallController, answerCall, CTCallRef, call) {
-    NSLog(@"-[<MPIncomingPhoneCallController: %p> answerCall:%@]", self, call);
+CHOptimizedMethod(0, self, void, MPIncomingPhoneCallController, dealloc) {
     [manager teardownForController:self];
-    CHSuper(1, MPIncomingPhoneCallController, answerCall, call);
+    CHSuper(0, MPIncomingPhoneCallController, dealloc);
 }
 
 CHOptimizedMethod(0, self, UIView *, MPIncomingPhoneCallController, newTopBar) {
-    NSLog(@"-[<MPIncomingPhoneCallController: %p> newTopBar]", self);
-    UIView *ret = CHSuper(0, MPIncomingPhoneCallController, newTopBar);
-    NSLog(@"%@", [manager viewForController:self]);
-    [ret addSubview:[manager viewForController:self]];
-    return ret;
-}
-
-CHOptimizedMethod(3, self, void, MPIncomingPhoneCallController, updateLCDWithName, id, name, label, id, _label, breakPoint, id, bp) {
-    NSLog(@"-[<MPIncomingPhoneCallController: %p> updateLCDWithName:%@ label:%@ breakPoint:%@]", self, name, _label, bp);
+    UIView *orig = CHSuper(0, MPIncomingPhoneCallController, newTopBar);
     [manager setForController:self ABUID:[self incomingCallerABUID]];
+    [orig addSubview:[manager viewForController:self]];
+    return orig;
+}
+
+CHOptimizedMethod(3, self, void, MPIncomingPhoneCallController, updateLCDWithName, NSString *, name, label, NSString *, aLabel, breakPoint, unsigned, aBreakPoint) {
     if ([manager hasViewForController:self]) {
-        _label = @" ";
+        aLabel = @" ";
     }
-    CHSuper(3, MPIncomingPhoneCallController, updateLCDWithName, name, label, _label, breakPoint, bp);
-}
-
-CHOptimizedMethod(0, self, void, MPIncomingPhoneCallController, ignore) {
-    NSLog(@"-[<MPIncomingPhoneCallController: %p> ignore]", self);
-    [manager teardownForController:self];
-    CHSuper(0, MPIncomingPhoneCallController, ignore);
-}
-
-CHDeclareClass(MPPhoneCallWaitingController);
-
-CHOptimizedMethod(0, self, void, MPPhoneCallWaitingController, ignore) {
-    NSLog(@"-[<MPPhoneCallWaitingController: %p> ignore]", self);
-    [manager teardownForController:self];
-    CHSuper(0, MPPhoneCallWaitingController, ignore);
-}
-
-CHOptimizedMethod(1, self, void, MPPhoneCallWaitingController, answerCall, CTCallRef, call) {
-    NSLog(@"-[<MPPhoneCallWaitingController: %p> answerCall:%@]", self, call);
-    [manager teardownForController:self];
-    CHSuper(1, MPPhoneCallWaitingController, answerCall, call);
+    CHSuper(3, MPIncomingPhoneCallController, updateLCDWithName, name, label, aLabel, breakPoint, aBreakPoint);
 }
 
 CHDeclareClass(SBPluginManager);
@@ -64,14 +39,9 @@ CHOptimizedMethod(1, self, Class, SBPluginManager, loadPluginBundle, NSBundle *,
     if ([[bundle bundleIdentifier] isEqualToString:@"com.apple.mobilephone.incomingcall"] && [bundle isLoaded]) {
         CHLoadLateClass(MPIncomingPhoneCallController);
         CHHook(1, MPIncomingPhoneCallController, initWithCall);
-        CHHook(1, MPIncomingPhoneCallController, answerCall);
+        CHHook(0, MPIncomingPhoneCallController, dealloc);
         CHHook(0, MPIncomingPhoneCallController, newTopBar);
         CHHook(3, MPIncomingPhoneCallController, updateLCDWithName, label, breakPoint);
-        CHHook(0, MPIncomingPhoneCallController, ignore);
-
-        CHLoadLateClass(MPPhoneCallWaitingController);
-        CHHook(1, MPPhoneCallWaitingController, answerCall);
-        CHHook(0, MPPhoneCallWaitingController, ignore);
     }
 
     return ret;
